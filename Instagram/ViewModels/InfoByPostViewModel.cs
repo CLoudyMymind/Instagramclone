9b71f2c248dd7358270dfd86c08115f0c3af9cc2
artemsek <sek39244@gmail.com>
2023-05-08 18:47:27 +0600

    Создана модель для просмотра постов и обновлены стили в лоялте

---
+++ Instagram/ViewModels/InfoByPostViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Instagram.Models;
+
+namespace Instagram.ViewModels;
+
+public class InfoByPostViewModel
+{
+    
+        public string Comment { get; set; }
+        public Post Post { get; set; }
+    
+}
