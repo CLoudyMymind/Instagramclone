d5012dc4e5f0a07804bc9823aa91360a198c8c08
artemsek <sek39244@gmail.com>
2023-05-08 18:44:32 +0600

    Создана модель подписок

---
+++ Instagram/Models/FollowersAndSubscribers.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Instagram.Models;
+
+public class FollowersAndSubscribers
+{
+    [Key]
+    public string Id { get; set; }
+    
+    public string FolowingUserId { get; set; }
+    public User FollowingUser { get; set; }
+    
+    public string FollowerUserId { get; set; }
+    public User FollowerUser { get; set; }
+
+    public DateTime CreateDate { get; set; }
+}
