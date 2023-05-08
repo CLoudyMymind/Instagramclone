ec90474848c6fe5e84a7fe297acfc6c13a87c479
artemsek <sek39244@gmail.com>
2023-05-08 18:40:21 +0600

    Создана вьюмодель для вьюшки для about

---
+++ Instagram/ViewModels/AboutsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Instagram.Models;
+
+namespace Instagram.ViewModels;
+
+public class AboutsViewModel
+{
+    public User User { get; set; }
+    public List<FollowersAndSubscribers> Followers { get; set; }
+    public List<FollowersAndSubscribers> Followings { get; set; }
+}
