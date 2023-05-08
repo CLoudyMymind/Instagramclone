4d26f47fcad7c41a6d481fa158209893c8aeb5bb
artemsek <sek39244@gmail.com>
2023-05-08 18:46:46 +0600

    Обновлены подключения

--- Instagram/Program.cs
+++ Instagram/Program.cs
@@ -1,9 +1,60 @@
+
+using Instagram.Models;
+using Instagram.services;
+using Instagram.services.Abstractions;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppContext = Instagram.Models.InstagramContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<InstagramContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 4;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.User.RequireUniqueEmail = true; 
+    })
+    // .AddErrorDescriber<RussianErrorDescriber>()
+    .AddEntityFrameworkStores<InstagramContext>();
+builder.Services.ConfigureApplicationCookie(o =>
+{
+    o.Cookie.HttpOnly = true;
+    o.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    o.SlidingExpiration = true;
+});
+builder.Services.Configure<CookieAuthenticationOptions>(options =>
+{
+    options.Cookie.Expiration = TimeSpan.Zero;
+    options.Cookie.SameSite = SameSiteMode.Strict;
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+
+// builder.Services.AddScoped<IdentityErrorDescriber, RussianErrorDescriber>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<ILikeService, LikeService>();
+builder.Services.AddScoped<IFollowingService, FollowingService>();
+// builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+});
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -22,6 +73,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
