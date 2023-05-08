54064d202eeb6681fc7733d83a566c21579fb5df
artemsek <sek39244@gmail.com>
2023-05-08 18:36:51 +0600

    Создан контроллер аккаунт и сервис для crud  операций

---
+++ Instagram/Controllers/AccountsController.cs
@@ -0,0 +1,352 @@
+using Instagram.Extensions;
+using Instagram.Models;
+using Instagram.services;
+using Instagram.services.Abstractions;
+using Instagram.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagram.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly IAccountService _accountService;
+    private readonly UserManager<User> _userManager;
+    private readonly IPostService _postService;
+    private readonly IFileService _fileService;
+    private readonly ICommentService _commentService;
+    private readonly ILikeService _likeService;
+    private readonly IFollowingService _followingService;
+    private readonly InstagramContext _instagramContext;
+    
+
+    public AccountController(
+        IAccountService accountService, 
+        UserManager<User> userManager,
+        IPostService postService,  
+        IFileService fileService,
+        ICommentService commentService, 
+        ILikeService likeService, IFollowingService followingService, InstagramContext instagramContext)
+    {
+        _accountService = accountService;
+        _userManager = userManager;
+        _postService = postService;
+        _fileService = fileService;
+        _commentService = commentService;
+        _likeService = likeService;
+        _followingService = followingService;
+        _instagramContext = instagramContext;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public  async Task<IActionResult> Index()
+    {
+       
+        return View(await _accountService.IndexPage(HttpContext.User));
+    }
+
+    public IActionResult SearchUser(SearchUserViewModel filter)
+    {
+        filter.UserName.ToUpper();
+        var str = _accountService.Search(filter);
+        var data = new IndexViewModel
+        {
+            Users = str.ToList()
+        };
+        return !str.Any() ? View(data) : View(data);
+    }
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var allowedMimeTypes = new[] { "image/jpeg", "image/jpg", "image/png" ,"image/webp" };
+            var mimeType = MimeTypes.GetMimeType(Path.GetExtension(model.Avatar.FileName));
+            if (!allowedMimeTypes.Contains(mimeType))
+            {
+                TempData["Error"] = "Вы загружаете формат который не позволен допустимые форматы jpg jpeg png";
+                return RedirectToAction("Register");
+            }
+            var imageUrl = Url.Content($"/images/{ await _fileService.FileRegisterCheck(model)}");
+            model.PathFile = imageUrl;
+            var result = await _accountService.RegisterUserAsync(model);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            foreach (var error in result.Errors)
+            {
+                if (error.Code == "DuplicateUserName")
+                {
+                    TempData["Error"] = "Этот никнейм уже занят используйте другой";
+                    return RedirectToAction("Register", "Account");
+                }
+                if (error.Code == "DuplicateEmail")
+                {
+                    TempData["Error"] = "Этот email уже занят используйте другой";
+                    return RedirectToAction("Register", "Account");
+                }
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        return View(model);
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Login(string? returnUrl)
+    {
+        return View(new LoginViewModel
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var isUserExists = await _accountService.IsUserExistsAsync(model);
+            if (isUserExists)
+            {
+                var passwordValid = await _accountService.CheckUserPasswordAsync(model, model.Password);
+                if (passwordValid)
+                {
+                    try
+                    {
+                        var signInResult = await _accountService.LoginUserAsync(model);
+                        if (signInResult.Succeeded)
+                        {
+                            if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                            {
+                                return Redirect(model.ReturnUrl);
+                            }
+
+                            return RedirectToAction("Index");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Неверный логин или пароль");
+        }
+        return View(model);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> LogOff()
+    {
+        await _accountService.SignOutUserAsync();
+        return RedirectToAction("Index");
+    }
+    [AllowAnonymous]
+    [HttpGet]
+    public IActionResult Edit(string id)
+    {
+        if (_accountService.GetAll().Any(s => s.Id == id))
+            try
+            {
+                var user = _accountService.GetById(id);
+                return View(new EditProfileViewModel()
+                {
+                    About = user.About,
+                    PathFile = user.PathFile,
+                    Name = user.Name,
+                    Avatar = user.Avatar,
+                    Email = user.Email,
+                    Password = user.PasswordHash,
+                    Phone = user.PhoneNumber,
+                    Login = user.UserName,
+                    Male = user.Male
+                });
+
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("About");
+            }
+
+        return NotFound();
+    }
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(EditProfileViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            if (model.Avatar == null)
+            {
+                var str = _accountService.GetAll().FirstOrDefault(p => p.Id == model.Id);
+                model.PathFile = str.PathFile;
+            }
+            else
+            {
+                var allowedMimeTypes = new[] { "image/jpeg", "image/jpg", "image/png" };
+                var mimeType = MimeTypes.GetMimeType(Path.GetExtension(model.Avatar.FileName));
+                if (!allowedMimeTypes.Contains(mimeType))
+                {
+                    TempData["Error"] = "Вы загружаете формат который не позволен допустимые форматы jpg jpeg png";
+                    return RedirectToAction("Edit" , new {Id = model.Id});
+                }
+                var imageUrl = Url.Content($"/images/{ await _fileService.FileEditCheck(model, HttpContext.User)}");
+                model.PathFile = imageUrl;
+            }
+            var result = await _accountService.Edit(model);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("About");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                if (error.Code == "DuplicateUserName")
+                {
+                    TempData["Error"] = "Этот никнейм уже занят используйте другой";
+                    return RedirectToAction("Register", "Account");
+                }
+                if (error.Code == "DuplicateEmail")
+                {
+                    TempData["Error"] = "Этот email уже занят используйте другой";
+                    return RedirectToAction("Register", "Account");
+                }
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+        return View(model);
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult PostsCreate()
+    {
+        return View(new CreatePostViewModel());
+    }
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> PostsCreate(CreatePostViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var allowedMimeTypes = new[] { "image/jpeg", "image/jpg", "image/png" , "image/webp"};
+            var mimeType = MimeTypes.GetMimeType(Path.GetExtension(model.Avatar.FileName));
+            if (!allowedMimeTypes.Contains(mimeType))
+            {
+                
+                TempData["Error"] = "Вы пытаетесь загрузить файл у которого разрешение которое не допустипо\n" +
+                                    "Допустимые форматы png jpeg jpg";
+                return View(model);
+            }
+            var imageUrl =  Url.Content($"/postImages/{await _fileService.FilePostCheck(model)}");
+            var userData = HttpContext.User;
+          await _postService.CreatePost(userData, model, imageUrl);
+        }
+        return RedirectToAction("About" );
+    }
+[HttpGet]
+public async Task<IActionResult> About(string Id)
+{
+    var viewModel = await _accountService.GetAboutViewModel(Id, User);
+    return View(viewModel);
+}
+
+
+[HttpGet]
+[AllowAnonymous]
+
+public IActionResult AboutsPostData(string id)
+{
+    if ( _postService.GetAll().Any(p=> p.Creater!.Id == id))
+    {
+        InfoByPostViewModel postComment = new() { Post = _postService.GetByIdPostAll(id) };
+        return View(postComment);
+    }
+    return NotFound();
+}
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddСomment(InfoByPostViewModel model)
+    {
+        try
+        {
+            await _commentService.Create(model, HttpContext.User);
+            return RedirectToAction("Index");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Произошла ошибка попробуйте в другой раз");
+            throw;
+        }
+    }
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Like(InfoByPostViewModel model)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            TempData["Error"] = "Такого пользователя нету";
+            return RedirectToAction("register");
+        }
+        var result = await _likeService.LikeAsync(model.Post.Id, user.Id);
+        if (result == false) return NotFound();
+        try
+        {
+            return RedirectToAction("index" );
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Такого айди нету в базе");
+            throw;
+        }
+    }
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> Follow(string id)
+    {
+        var isFollowed = await _followingService.FollowOrUnfollowUserAsync(id, User);
+        if (isFollowed == false)
+        {
+            TempData["Error"] = "Вы пытаетесь подписаться на себя";
+            return RedirectToAction("About", new { Id = id });
+        }
+        return RedirectToAction("About", new {id = id,});
+    }
+    
+    [HttpGet]
+    public IActionResult AllFollowersUser(string id)
+    {
+        return View(_accountService.FollowUsers(id));
+    }
+    [HttpGet]
+    public IActionResult AllFlowingsUsers(string id)
+    {
+        return View(_accountService.FlowingUsers(id));
+    }
+    
+    
+}
---
+++ Instagram/services/AccountService.cs
@@ -0,0 +1,271 @@
+using System.Security.Claims;
+using Instagram.Extensions;
+using Instagram.Models;
+using Instagram.services.Abstractions;
+using Instagram.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagram.services;
+
+public class AccountService : IAccountService
+{
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    private readonly InstagramContext _instagramContext;
+
+    public AccountService(
+        UserManager<User> userManager,
+        SignInManager<User> signInManager, InstagramContext instagramContext)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _instagramContext = instagramContext;
+    }
+
+    public async Task<IdentityResult> RegisterUserAsync(RegisterViewModel model)
+    {
+        
+        User user = new()
+        {
+            Email = model.Email,
+            UserName = model.Login,
+            Male = model.Male,
+            Name = model.Name,
+            PhoneNumber = model.Phone,
+            About = model.About,
+            PathFile = model.PathFile!
+        };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, false);
+        }
+        return result;
+    }
+
+    public async Task<SignInResult> LoginUserAsync(LoginViewModel model)
+    {
+        if (model.Email != null)
+        {
+            User? user = await _userManager.FindByEmailAsync(model.Email);
+            SignInResult signInResult = await _signInManager.PasswordSignInAsync(
+                user,
+                model.Password,
+                model.RememberMe,
+                false
+            );
+            return signInResult;
+        }
+
+        if (model.Name != null)
+        {
+            User? user = await _userManager.FindByNameAsync(model.Name);
+            SignInResult signInResult = await _signInManager.PasswordSignInAsync(
+                user,
+                model.Password,
+                model.RememberMe,
+                false
+            );
+            return signInResult;
+        }
+
+        throw new Exception("Такого пользователя нету");
+
+
+    }
+
+    public async Task SignOutUserAsync()
+    {
+        await _signInManager.SignOutAsync();
+    }
+
+    public async Task<bool> IsUserExistsAsync(LoginViewModel email)
+    {
+        if (email.Email is not null)
+        {
+            User? user = await _userManager.FindByEmailAsync(email.Email);
+            return user != null;
+
+        }
+
+        if (email.Name is not null)
+        {
+            User? user = await _userManager.FindByNameAsync(email.Name);
+            return user != null;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> CheckUserPasswordAsync(LoginViewModel model, string password)
+    {
+        if (model.Email != null)
+        {
+            User? user = await _userManager.FindByEmailAsync(model.Email);
+            bool passwordValid = await _userManager.CheckPasswordAsync(user, password);
+            return passwordValid;
+
+        }
+
+        if (model.Name != null)
+        {
+            User? user = await _userManager.FindByNameAsync(model.Name);
+            bool passwordValid = await _userManager.CheckPasswordAsync(user, password);
+            return passwordValid;
+        }
+
+        return false;
+    }
+
+    public async Task<List<string?>> CheсkEmail(string email)
+    {
+        var allUsers = await _userManager.Users.ToListAsync();
+        var emails = allUsers.Select(u => u.Email).ToList();
+        return emails;
+    }
+
+
+
+    public List<User> GetAll() => _instagramContext.Users.Include(p => p.Posts).ToList();
+
+    public User? GetById(string id) =>  _instagramContext.Users.FirstOrDefault(u => u.Id == id);
+    
+    public async Task<IdentityResult> Edit(EditProfileViewModel model)
+    {
+        
+        var fist = _instagramContext.Users.FirstOrDefault(p => p.Id == model.Id);
+        if (fist is not null)
+        {
+            fist.Id = model.Id;
+            fist.About = model.About;
+            fist.Email = model.Email;
+            fist.Name = model.Name;
+            fist.Male = model.Male;
+            fist.UserName = model.Login;
+            fist.PasswordHash = model.Password;
+            fist.PasswordHash = model.ConfirmPassword;
+
+        }
+        _instagramContext.Update(fist);
+
+        IdentityResult result = await _userManager.UpdateAsync(fist);
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(fist, false);
+        }
+        return result;
+    }
+
+    public IQueryable<User> GetQueryableUsers()
+    {
+        return _instagramContext.Users;
+    }
+    
+
+    public List<User> AllUsersAbout(ClaimsPrincipal? user)
+    {
+        var userId = _userManager.GetUserId(user);
+        var usersWithPosts = _instagramContext.Users.Include(u => u.Posts).FirstOrDefault(u => u.Id == userId);
+        return new List<User> { usersWithPosts };
+    }
+    private IQueryable<FollowersAndSubscribers> GetFollowersAndSubscribersForUserQuery(string userId)
+    {
+        return _instagramContext.FollowersAndSubscribers
+            .Include(follow => follow.FollowerUser)
+            .Include(follow => follow.FollowingUser)
+            .Where(follow => follow.FollowingUser.Id == userId || follow.FollowerUser.Id == userId);
+    }
+
+    public async Task<AboutsViewModel> GetAboutViewModel(string? id, ClaimsPrincipal user)
+    {
+        User appUser = id is null ? await _userManager.GetUserAsync(user) : await _userManager.FindByIdAsync(id);
+        appUser.Posts = await _instagramContext.Posts
+            .Include(post => post.Likes)
+            .Include(post => post.Comments)
+            .Where(post => post.Creater!.Id == appUser.Id)
+            .ToListAsync();
+
+        var follow = await GetFollowersAndSubscribersForUserQuery(appUser.Id)
+            .Where(follow => follow.FollowingUser.Id == appUser.Id)
+            .ToListAsync();
+
+        var following = await GetFollowersAndSubscribersForUserQuery(appUser.Id)
+            .Where(follow => follow.FollowerUser.Id == appUser.Id)
+            .ToListAsync();
+        return new AboutsViewModel
+        {
+            User = appUser,
+            Followers = follow,
+            Followings = following
+        };
+    }
+
+    public List<User> Search(SearchUserViewModel filter)
+    {
+
+        var str = _instagramContext.Users
+            .WhereIf(!string.IsNullOrEmpty(filter.UserName), u =>
+                u.UserName.Contains(filter.UserName)
+                || u.Email.Contains(filter.UserName)
+                || (u.Name != null && u.Name.Contains(filter.UserName))).ToList();
+        return str.ToList();
+    }
+
+    public List<FollowersAndSubscribers> FollowUsers(string id)
+    {
+        return GetFollowersAndSubscribersQuery(id, true)
+            .Include(follow => follow.FollowingUser)
+            .OrderByDescending(c => c.CreateDate)
+            .ToList();
+    }
+    
+    public List<FollowersAndSubscribers> FlowingUsers(string id)
+    {
+        return GetFollowersAndSubscribersQuery(id, false)
+            .Include(follow => follow.FollowerUser)
+            .OrderByDescending(c => c.CreateDate)
+            .ToList();
+    }
+
+    private IQueryable<FollowersAndSubscribers> GetFollowersAndSubscribersQuery(string id, bool searchFollowers)
+    {
+        var query = _instagramContext.FollowersAndSubscribers
+            .Where(follow => searchFollowers ? follow.FollowerUser.Id == id : follow.FollowingUser.Id == id);
+
+        return query;
+    }
+    public async Task<IndexViewModel> IndexPage(ClaimsPrincipal user)
+    {
+        var userIdentity =  await _userManager.GetUserAsync(user);
+
+        var followedUsers = _instagramContext.FollowersAndSubscribers
+            .Include(follow => follow.FollowingUser)
+            .Include(follow => follow.FollowerUser.Posts)
+            .ThenInclude(post => post.Likes)
+            .Include(follow => follow.FollowerUser.Posts)
+            .ThenInclude(post => post.Comments)
+            .Where(follow => follow.FollowingUser.Id == userIdentity!.Id)
+            .ToList();
+
+        var allUser = _instagramContext.Users.ToList();
+
+        var followingUser = _instagramContext.FollowersAndSubscribers
+            .Where(follow => follow.FolowingUserId == userIdentity!.Id)
+            .ToList();
+
+        foreach (var followerUser in followingUser)
+        {
+            var orDefault = allUser.FirstOrDefault(user1 => user1.Id == followerUser.FollowerUserId);
+            allUser.Remove(orDefault!);
+        }
+        
+        allUser.Remove(userIdentity!);
+        IndexViewModel model = new()
+        {
+            Follows = followedUsers,
+            Users = allUser
+        };
+        return model;
+    }
+}
