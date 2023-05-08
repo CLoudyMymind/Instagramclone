f024c7af110e6d057b7c6ab1211cfa1b153de99f
artemsek <sek39244@gmail.com>
2023-05-08 18:46:22 +0600

    Создана вьюмодель для главной страницы

---
+++ Instagram/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Instagram.Models;
+
+namespace Instagram.ViewModels;
+
+public class IndexViewModel
+{
+    public List<User> Users { get; set; }
+
+    public SearchUserViewModel SearchUserViewModel { get; set; }
+    
+    public string Comment { get; set; }
+
+    public List<FollowersAndSubscribers> Follows { get; set; }
+    public Post Post { get; set; }
+}
