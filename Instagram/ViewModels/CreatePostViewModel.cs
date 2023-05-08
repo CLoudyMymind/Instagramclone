a0ad7762f18a15f6363f0c0b659347b5b2abee44
artemsek <sek39244@gmail.com>
2023-05-08 18:45:28 +0600

    СОздана вью модель для создания постов

---
+++ Instagram/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Instagram.Attributes;
+
+namespace Instagram.ViewModels;
+
+public class CreatePostViewModel
+{
+    public Guid Id { get; set; }
+
+    [DataType(DataType.Upload, ErrorMessage = "загрузите фотку")]
+    [Required(ErrorMessage = "Загрузите Обложку")]
+    [Display(Name = "Загрутите Обложку")]
+    [AllowedExtensions(new []{".png" , ".jpeg" , "jpg" , "webp" , "ico" , "svg"})]
+    public IFormFile Avatar { get; set; }
+    [Required(ErrorMessage = "Заполните Инофрмацию о посте")]
+    [StringLength(150, MinimumLength = 1, ErrorMessage = "Минимальное чисто символов: 1, Максимальное 150")]
+    [Display(Name = "Укажите информацию о посте")]
+    public string Decription { get; set; }
+
+    public string? PathFile { get; set; }
+}
