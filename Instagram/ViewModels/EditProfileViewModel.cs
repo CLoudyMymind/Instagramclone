b637e3af5ee738d98190c0d6ab7b4201f6c8f128
artemsek <sek39244@gmail.com>
2023-05-08 18:45:12 +0600

    Создана вьюмодель и вьюшка для изменения данных юзера

---
+++ Instagram/ViewModels/EditProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Instagram.Models;
+
+namespace Instagram.ViewModels;
+
+public class EditProfileViewModel
+{
+    public string Id { get; set; }
+    public IFormFile? Avatar { get; set; }
+    
+    public string? PathFile { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? About { get; set; }
+
+    public string? Male { get; set; }
+    
+    public string? Phone { get; set; }
+
+    public string Password { get; set; }
+
+    public string Email { get; set; }
+
+    public string Login { get; set; }
+    
+    public string ConfirmPassword { get; set; }
+}
