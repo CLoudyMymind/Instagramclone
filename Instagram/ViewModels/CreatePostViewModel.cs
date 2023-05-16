60b6d803e378daf0b5e468673b257cc16b386579
Артем <Sek39244@gmail.com>
2023-05-16 19:57:28 +0600

    Добавлено свойство поста

--- Instagram/ViewModels/CreatePostViewModel.cs
+++ Instagram/ViewModels/CreatePostViewModel.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Instagram.Attributes;
+using Instagram.Models;
 
 namespace Instagram.ViewModels;
 
 public class CreatePostViewModel
 {
-    public Guid Id { get; set; }
-
     [DataType(DataType.Upload, ErrorMessage = "загрузите фотку")]
     [Required(ErrorMessage = "Загрузите Обложку")]
     [Display(Name = "Загрутите Обложку")]
-    [AllowedExtensions(new []{".png" , ".jpeg" , "jpg" , "webp" , "ico" , "svg"})]
+    [AllowedExtensions(new []{".png" , ".jpeg" , ".jpg" , ".webp" , ".ico" , ".svg"})]
     public IFormFile Avatar { get; set; }
     [Required(ErrorMessage = "Заполните Инофрмацию о посте")]
     [StringLength(150, MinimumLength = 1, ErrorMessage = "Минимальное чисто символов: 1, Максимальное 150")]
@@ -18,4 +17,6 @@ public class CreatePostViewModel
     public string Decription { get; set; }
 
     public string? PathFile { get; set; }
+
+    public Post? Post { get; set; }
 }
