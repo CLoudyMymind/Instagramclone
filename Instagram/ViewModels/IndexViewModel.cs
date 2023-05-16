842cdd49eb60218d448b87f68184214b6a230581
Артем <Sek39244@gmail.com>
2023-05-16 19:58:45 +0600

    Добавлены атрибуты для валидации коментария

--- Instagram/ViewModels/IndexViewModel.cs
+++ Instagram/ViewModels/IndexViewModel.cs
@@ -1,15 +1,18 @@
-﻿using Instagram.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Instagram.Models;
 
 namespace Instagram.ViewModels;
 
 public class IndexViewModel
 {
-    public List<User> Users { get; set; }
+    public List<User>? Users { get; set; }
 
-    public SearchUserViewModel SearchUserViewModel { get; set; }
-    
+    public SearchUserViewModel? SearchUserViewModel { get; set; }
+    [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
+    [Required(ErrorMessage = "Заполните Коментарий")]
     public string Comment { get; set; }
 
-    public List<FollowersAndSubscribers> Follows { get; set; }
-    public Post Post { get; set; }
+    public List<FollowersAndSubscribers>? Follows { get; set; }
+    public Post? Post { get; set; }
+
 }
--- Instagram/ViewModels/InfoByPostViewModel.cs
+++ Instagram/ViewModels/InfoByPostViewModel.cs
@@ -1,11 +1,16 @@
-﻿using Instagram.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Instagram.Models;
 
 namespace Instagram.ViewModels;
 
 public class InfoByPostViewModel
 {
     
+        [DataType(DataType.Text , ErrorMessage = "тут должен быть только текст")]
+        [Required(ErrorMessage = "Заполните коментарий")]
         public string Comment { get; set; }
-        public Post Post { get; set; }
+        public Post? Post { get; set; }
+        public User? User { get; set; }
+
     
 }
