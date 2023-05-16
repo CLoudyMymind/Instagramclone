fab6b813b95e6f57f3f3483b5bc60f0b859e4442
Артем <Sek39244@gmail.com>
2023-05-16 19:58:10 +0600

    Удалены ненужные методы в сервисе

--- Instagram/services/Abstractions/IAccountService.cs
+++ Instagram/services/Abstractions/IAccountService.cs
@@ -11,24 +11,8 @@ public interface IAccountService
 
     public  Task<SignInResult> LoginUserAsync(LoginViewModel model);
     public Task SignOutUserAsync();
-
     public Task<bool> IsUserExistsAsync(LoginViewModel email);
-
     public Task<bool> CheckUserPasswordAsync(LoginViewModel model, string password);
-
-    Task<List<string?>> CheсkEmail(string email);
-    
-    List<User> GetAll();
-
-   User? GetById(string id);
-   public Task<IdentityResult> Edit(EditProfileViewModel model);
-   IQueryable<User> GetQueryableUsers();
-
-   Task<AboutsViewModel> GetAboutViewModel(string? id, ClaimsPrincipal user);
-   public List<User> AllUsersAbout(ClaimsPrincipal? user);
-   List<User> Search(SearchUserViewModel filter);
-
-   List<FollowersAndSubscribers> FollowUsers(string id);
-   List<FollowersAndSubscribers> FlowingUsers(string id);
-   Task<IndexViewModel> IndexPage(ClaimsPrincipal user);
+    List<User> Search(SearchUserViewModel filter);
+    Task<IndexViewModel> IndexPage(ClaimsPrincipal user);
 }
