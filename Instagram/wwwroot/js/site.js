f7e37567188066d79f372b2a0149075ef678a2e0
artemsek <sek39244@gmail.com>
2023-05-18 17:54:06 +0600

    написал скрипты

--- Instagram/wwwroot/js/site.js
+++ Instagram/wwwroot/js/site.js
@@ -1,4 +1,49 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function() {
+    $(document).on('click', '.subscribe-link', function(event) {
+        event.preventDefault();
+        var userId = $(this).attr('userId');
+        var folowersCount = $('.profile-following').text();
+        console.log(userId);
 
-// Write your JavaScript code.
+        var requestVerificationToken = $('input[name="__RequestVerificationToken"]').val();
+        var headers = {
+            "RequestVerificationToken": requestVerificationToken
+        };
+
+        $.ajaxSetup({
+            headers: headers
+        });
+
+        var subscribeLink = $(this);
+
+        $.post('/Profiles/Follow/', {id: userId})
+            .done(function(response) {
+                if (!subscribeLink.hasClass('subscribed')) {
+                    subscribeLink.text('Отписаться').addClass('subscribed btn btn-outline-danger');
+                    $('.profile-following').text(parseInt(folowersCount) + 1);
+                } else {
+                    subscribeLink.text('Подписаться').removeClass('subscribed btn btn-outline-danger').addClass('btn btn-outline-primary');
+                    $('.profile-following').text(parseInt(folowersCount) - 1);
+                }
+            })
+            .fail(function(error) {
+                console.log(error);
+            });
+    });
+});
+var deleteButton = document.getElementById('deleteButton');
+deleteButton.addEventListener('click', function(e) {
+    e.preventDefault(); 
+
+    var postId = deleteButton.getAttribute('data-post-id'); 
+    $.ajax({
+        url: '/Posts/DeletePost',
+        type: 'POST',
+        data: { id: postId }, 
+        success: function(result) {
+            if (result.success) {
+                deleteButton.closest('.post').remove();
+            }
+        }
+    });
+});
